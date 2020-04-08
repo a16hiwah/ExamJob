58cd87d3b243337bb213108d0c1128dccf3248d2
a16hiwah <37792852+a16hiwah@users.noreply.github.com>
2020-04-08 13:03:03 +0200

    add php  microtime function for measurement

--- Cakephp/src/Controller/AppController.php
+++ Cakephp/src/Controller/AppController.php
@@ -13,7 +13,8 @@
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace App\Controller;
-
+// Place at the top of index.php (first file to load)
+$time_start = microtime(true);
 use Cake\Controller\Controller;
 use Cake\Event\Event;
 
@@ -89,3 +90,22 @@ class AppController extends Controller
         }
     }
 }
+
+// Sleep for a while
+//usleep(1000);
+// Place all the code below at the bottom of index.php (first file to load)
+// except the PHP closing tag
+$time_end = microtime(true);
+
+// Calculate execution time in milliseconds (round to 3 decimals)
+$time = round($time_end - $time_start,5);
+//$exec_time = round((($t_stop - $t_start) * 1000), 3);
+echo "Did nothing in $time seconds\n";
+// Path to csv file where the result should be saved (choose one)
+//$fileLocation = getenv('DOCUMENT_ROOT') . '/logs/codeigniter_measurements.csv';
+ $fileLocation = getenv('DOCUMENT_ROOT') . '/logs/cakephp_measurements.csv';
+
+// Save the result
+$handle = fopen($fileLocation, 'a');
+fputcsv($handle, [$time]);
+fclose($handle);
