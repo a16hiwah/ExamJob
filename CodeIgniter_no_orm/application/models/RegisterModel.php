ea530f276bb06e70d7d969ae6d5fae9bd02d7e5a
a16hiwah <37792852+a16hiwah@users.noreply.github.com>
2020-04-08 14:39:31 +0200

    change way to insert data to users table to raw querycql

--- CodeIgniter_no_orm/application/models/RegisterModel.php
+++ CodeIgniter_no_orm/application/models/RegisterModel.php
@@ -3,6 +3,14 @@
 class RegisterModel extends CI_Model{
     public function add_user($data){
         //get the data from controller and insert into the table 'users'
-        return $this->db->insert('users', $data);
+        //return $this->db->insert('users', $data); //codeiginter way to insert data into database
+		$name = $data['name'];
+        $email = $data['email'];
+        $password = $data['password'];
+        $created = $data['created'];
+        $modified = $data['modified'];
+        $sql = "INSERT INTO users(name, email, password, created, modified) VALUES('$name', '$email', '$password', '$created', '$modified')";
+        $query = $this->db->query($sql);
+        return $query;
     }
 }
