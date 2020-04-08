64b6076e424b0813900b53c7de1c807a3737f869
a16hiwah <37792852+a16hiwah@users.noreply.github.com>
2020-04-08 14:56:20 +0200

    checking login use SELECT raw mysql query
    checking login useing raw Sql query

--- CodeIgniter_no_orm/application/models/LoginModel.php
+++ CodeIgniter_no_orm/application/models/LoginModel.php
@@ -3,10 +3,13 @@
 class LoginModel extends CI_Model {
 
     public function checkLogin($email, $password) {
-        //query the table 'users' and get the result count
-        $this->db->where('email', $email);
-        $this->db->where('password', $password);
-        $query = $this->db->get('users');
+         //query the table 'users' and get the result count
+        // $this->db->where('email', $email);
+        // $this->db->where('password', $password);
+        // $query = $this->db->get('users');
+        $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+        $query = $this->db->query($sql);
+            //return $query;
         return $query->num_rows();
     }
 
