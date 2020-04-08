374cd697da9a1430becf716141edbbc923f9eb26
a16hiwah <37792852+a16hiwah@users.noreply.github.com>
2020-04-08 15:23:46 +0200

    useing raw sql query to connect to database
    useing raw sql query to connect to database instead of cakephp query.
    query like SELECT, insert, update, inlog control

--- cakephp_on_orm/src/Controller/UsersController.php
+++ cakephp_on_orm/src/Controller/UsersController.php
@@ -1,8 +1,9 @@
 <?php
 namespace App\Controller;
-
 use App\Controller\AppController;
 use Cake\Event\Event;
+use Cake\Datasource\ConnectionManager;
+use Cake\Auth\DefaultPasswordHasher;
 
 /**
  * Users Controller
@@ -20,9 +21,19 @@ class UsersController extends AppController
      */
     public function index()
     {
-        $users = $this->paginate($this->Users);
+        $conn = ConnectionManager::get('default');
+        // $limit = $this->request->getQuery('limit');
+        // $offset = $this->request->getQuery('offset');       
+        $users = $conn->execute("SELECT * FROM users")->fetchAll('assoc');
+        // print_r($users);
+        // $user = $this->paginate($this->Users);
+        // print_r($user);
+        // exit();
 
-        $this->set(compact('users'));
+        // $this->set(compact('users'));
+        
+
+        $this->set('users',$users);
     }
 
     /**
@@ -34,10 +45,17 @@ class UsersController extends AppController
      */
     public function view($id = null)
     {
-        $user = $this->Users->get($id, [
-            'contain' => ['Posts'],
-        ]);
+        // $user = $this->Users->get($id, [
+        //     'contain' => ['Posts'],
+        // ]);
 
+        // $this->set('user', $user);
+
+        $conn = ConnectionManager::get('default');
+        
+        $user = $conn->execute("SELECT a.*, b.id as pid, b.title, b.body, b.user_id, b.created as pcrt, b.modified as pmode FROM users a LEFT JOIN posts b ON a.id = b.user_id WHERE a.id = $id ")->fetchAll('assoc');
+
+      
         $this->set('user', $user);
     }
 
@@ -49,14 +67,52 @@ class UsersController extends AppController
     public function add()
     {
         $user = $this->Users->newEntity();
-        if ($this->request->is('post')) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
-            if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
+        // if ($this->request->is('post')) {
+        //     $user = $this->Users->patchEntity($user, $this->request->getData());
+        //     if ($this->Users->save($user)) {
+        //         $this->Flash->success(__('The user has been saved.'));
 
+        //         return $this->redirect(['action' => 'index']);
+        //     }
+        //     $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        // }
+        if($this->request->is('post')){
+            
+            $postd = $this->request->getData();
+
+            $name = $postd['name'];
+            $email = $postd['email'];
+            $password = $postd['password'];
+            $hasher = new DefaultPasswordHasher();
+            $modified = date('Y-m-d H:i:s');
+            $created =  date('Y-m-d H:i:s');
+
+            //print_r($postd);
+            //exit();
+        
+            $conn = ConnectionManager::get('default');
+            //$auth = $conn->execute("SELECT email FROM users WHERE email = '$email'")->fetchAll('assoc');
+            // print_r($auth);
+            // exit();
+
+            if($conn->execute("SELECT email FROM users WHERE email = '$email'")->fetchAll('assoc')){
+                
+                $this->Flash->error(__('The user could not be saved. Please, try again.'));
+                
+            }
+            else{
+                $conn->insert('users', [
+                'name' => $name,
+                'email' => $email,
+                'password' => $hasher->hash($password),
+                'modified' => $modified,
+                'created' => $created
+                 ], ['created' => 'datetime']);
+                //$conn->insert("INSERT INTO posts VALUES('',$title,$body,$uid,$modified,$created)");
+
+                $this->Flash->success(__('The user has been saved.'));
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The user could not be saved. Please, try again.'));
         }
         $this->set(compact('user'));
     }
@@ -73,14 +129,47 @@ class UsersController extends AppController
         $user = $this->Users->get($id, [
             'contain' => [],
         ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
-            if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
+        // if ($this->request->is(['patch', 'post', 'put'])) {
+        //     $user = $this->Users->patchEntity($user, $this->request->getData());
+        //     if ($this->Users->save($user)) {
+        //         $this->Flash->success(__('The user has been saved.'));
 
+        //         return $this->redirect(['action' => 'index']);
+        //     }
+        //     $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        // }
+        if($this->request->getData() == TRUE){
+            
+            $postd = $this->request->getData();
+
+            $name = $postd['name'];
+            $email = $postd['email'];
+            $password = $postd['password'];
+            $hasher = new DefaultPasswordHasher();
+            $modified = date('Y-m-d H:i:s');
+            // $created =  date('Y-m-d H:i:s');
+
+            //print_r($postd);
+            //exit();
+        
+            $conn = ConnectionManager::get('default');
+
+            if($name != ""){
+                
+                $conn->update('users', [
+                'name' => $name,
+                'email' => $email,
+                'password' => $hasher->hash($password),
+                'modified' => $modified
+                 ], ['id' => $id]);
+                //$conn->insert("INSERT INTO posts VALUES('',$title,$body,$uid,$modified,$created)");
+
+                $this->Flash->success(__('The user has been saved.'));
                 return $this->redirect(['action' => 'index']);
             }
-            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+            else{
+                $this->Flash->error(__('The user could not be saved. Please, try again.'));
+            }
         }
         $this->set(compact('user'));
     }
@@ -96,7 +185,8 @@ class UsersController extends AppController
     {
         $this->request->allowMethod(['post', 'delete']);
         $user = $this->Users->get($id);
-        if ($this->Users->delete($user)) {
+        $conn = ConnectionManager::get('default');
+        if ($conn->delete('users', ['id' => $id])) {
             $this->Flash->success(__('The user has been deleted.'));
         } else {
             $this->Flash->error(__('The user could not be deleted. Please, try again.'));
@@ -109,13 +199,26 @@ class UsersController extends AppController
      //validation, inloggningskontroll kollar användaren har rätt login
      public function login(){
         if($this->request->is('post')){   
-            $user = $this->Auth->identify();
-            if($user){
-                $this->Auth->setUser($user);
+            $check = $this->request->getData();
+            $hasher = new DefaultPasswordHasher();
+            
+
+            $email = $check['email'];
+            $pwd = $check['password'];
+            $conn = ConnectionManager::get('default');
+            $user = $conn->execute("SELECT * FROM users WHERE email = '$email'")->fetchAll('assoc');
+            //print_r($user);
+            $npwd = $user[0]['password'];
+            //exit();
+            if($hasher->check($pwd,$npwd)){
+                $this->Auth->setUser($user[0]);
                 return $this->redirect(['controller' => 'posts']);
             }
-            // fel Login meddelande
-            $this->Flash->error('Incorrect Login');
+            else{
+                // fel Login meddelande
+                $this->Flash->error('Incorrect Login');
+            }
+            
         }
     }
 
@@ -128,12 +231,30 @@ class UsersController extends AppController
    public function register(){
     $user = $this->Users->newEntity();
     if($this->request->is('post')){
-        $user = $this->Users->patchEntity($user, $this->request->data);
-        if($this->Users->save($user)){
-            $this->Flash->success('You are registered and can login');
-            return $this->redirect(['action' => 'login']);
-        } else {
-            $this->Flash->error('You are not registered');
+        
+        $postd = $this->request->getData();
+
+        $name = $postd['name'];
+        $email = $postd['email'];
+        $password = $postd['password'];
+        $hasher = new DefaultPasswordHasher();
+        $modified = date('Y-m-d H:i:s');
+        $created =  date('Y-m-d H:i:s');
+        $conn = ConnectionManager::get('default');
+        if($conn->execute("SELECT email FROM users WHERE email = '$email'")->fetchAll('assoc')){
+
+            $this->Flash->error(__('You are not registered'));
+
+        }
+        else{
+            $conn->insert('users', [
+            'name' => $name,
+            'email' => $email,
+            'password' => $hasher->hash($password),
+            'modified' => $modified,
+            'created' => $created ], ['created' => 'datetime']);
+            $this->Flash->success(__('You are registered and can login'));
+            return $this->redirect(['action' => 'index']);
         }
     }
     $this->set(compact('user'));
