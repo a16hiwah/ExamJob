0290d7cf6944462a656f8b95da89beb1478c0332
a16hiwah <37792852+a16hiwah@users.noreply.github.com>
2020-04-08 15:02:45 +0200

    use SELECT, INSERT, UPDATE & DELET raw mysql query to users table
    use SELECT, INSERT, UPDATE & DELET raw mysql query to get datainstead of condeigniter to users tabel

--- CodeIgniter_no_orm/application/models/user/UserModel.php
+++ CodeIgniter_no_orm/application/models/user/UserModel.php
@@ -13,7 +13,12 @@ class UserModel extends CI_Model{
             $offset = ($offset - 1) * $limit;
         }
         
-        $result['rows'] = $this->db->get('users', $limit, $offset); //get data from users table using limit and offset
+        // $result['rows'] = $this->db->get('users', $limit, $offset); //get data from users table using limit and offset
+        // $result['num_rows'] = $this->db->count_all_results('users'); //count all data from table users
+        $query1 = "SELECT * FROM users LIMIT $limit OFFSET $offset";
+        $query2 = "SELECT COUNT(*) FROM users";
+
+        $result['rows'] = $this->db->query($query1); //get data from users table using limit and offset
         $result['num_rows'] = $this->db->count_all_results('users'); //count all data from table users
         
         return $result; //return value to controller
@@ -21,28 +26,39 @@ class UserModel extends CI_Model{
 
     public function add_new_user($data){
         //get the data from controller and insert into the table 'users'
-        return $this->db->insert('users', $data);
+        $name = $data['name'];
+        $email = $data['email'];
+        $password = $data['password'];
+        $created = $data['created'];
+        $modified = $data['modified'];
+        $sql = "INSERT INTO users(name, email, password, created, modified) VALUES('$name', '$email', '$password', '$created', '$modified')";
+        $query = $this->db->query($sql);
+        return $query;
     }
 
     public function get_single_user($id){
             
             //get all the value of id(send from controller) from users using query and return to controller
-            $this->db->select('*'); 
-            $this->db->from('users');            
-            $this->db->where('id', $id);
-            $query = $this->db->get();
+            $sql = "SELECT * FROM users WHERE id = '$id'";
+            $query = $this->db->query($sql);
             return $query->row_array();
     }
 
     public function updateuser($user, $id){
             //search the id in users table which sends from controller and update the rows with new users values
-            $this->db->where('users.id', $id);
-            return $this->db->update('users', $user);
+            $name = $user['name'];
+            $email = $user['email'];
+            //$password = $user['password'];
+            $modified = $user['modified'];
+            $sql = "UPDATE users SET name = '$name', email = '$email', modified = '$modified' WHERE id = '$id'";
+            $query = $this->db->query($sql);
+            return $query;
     }
 
     public function deleteuser($id){
             //delete the entire row of id from users table which sends from controller 
-            $this->db->where('users.id', $id);
-            return $this->db->delete('users');
+            $sql = "DELETE FROM users WHERE id = '$id'";
+            $query = $this->db->query($sql);
+            return $query;
     }
 }
