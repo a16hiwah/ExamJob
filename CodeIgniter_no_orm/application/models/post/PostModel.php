c5127a11d471a0c26e1a9fff48269349498038a4
a16hiwah <37792852+a16hiwah@users.noreply.github.com>
2020-04-08 14:57:14 +0200

    use SELECT, INSERT, UPDATE & DELET raw mysql query to posts table
    use SELECT, INSERT, UPDATE & DELET raw mysql query to get datainstead of condeigniter to posts tabel

--- CodeIgniter_no_orm/application/models/post/PostModel.php
+++ CodeIgniter_no_orm/application/models/post/PostModel.php
@@ -7,38 +7,45 @@ class PostModel extends CI_Model{
             $offset = ($offset - 1) * $limit;
         }
 		
-        $result['rows'] = $this->db->get('posts', $limit, $offset); //get data from posts table using limit and offset
-        $result['num_rows'] = $this->db->count_all_results('posts'); //count all data from table posts
+        // $result['rows'] = $this->db->get('posts', $limit, $offset); //get data from posts table using limit and offset
+        // $result['num_rows'] = $this->db->count_all_results('posts'); //count all data from table posts
+        $query1 = "SELECT * FROM posts LIMIT $limit OFFSET $offset";
+        $query2 = "SELECT COUNT(*) FROM posts";
+
+        $result['rows'] = $this->db->query($query1); //get data from users table using limit and offset
+        $result['num_rows'] = $this->db->count_all_results('posts'); //count all data from table users
 		
         return $result; //return value to controller
     }
 
     public function add_new_post($data){
         //get the data from controller and insert into the table 'posts'
-        return $this->db->insert('posts', $data);
+        $title = $data['title'];
+        $body = $data['body'];
+        $user_id = $data['user_id'];
+        $created = $data['created'];
+        $modified = $data['modified'];
+        $sql = "INSERT INTO posts(title, body, user_id, created, modified) VALUES('$title', '$body', '$user_id', '$created', '$modified')";
+        $query = $this->db->query($sql);
+        return $query;
     }
 
-    public function get_single_post($id){
-			//create a join query using the data send from controller
-			$this->db->select('a.*,b.name'); 
-            $this->db->from('posts a');            
-            $this->db->join('users b', 'b.id = a.user_id', 'left'); 
-            $this->db->where('a.id', $id);
-            //get the value from tables using join query and return to controller
-            $query = $this->db->get();
-            return $query->row_array();
-	}
-
-	public function updatepost($post, $id){
+    public function updatepost($post, $id){
 		    //search the id in posts table which sends from controller and update the rows with new post values
-			$this->db->where('posts.id', $id);
-			return $this->db->update('posts', $post);
+			$title = $post['title'];
+            $body = $post['body'];
+            $user_id = $post['user_id'];
+            $modified = $post['modified'];
+            $sql = "UPDATE posts SET title = '$title', body = '$body', user_id = '$user_id', modified = '$modified' WHERE id = '$id'";
+            $query = $this->db->query($sql);
+            return $query;
 	}
 
 	public function deletepost($id){
 		    //delete the entire row of id from posts table which sends from controller 
-			$this->db->where('posts.id', $id);
-			return $this->db->delete('posts');
+			$sql = "DELETE FROM posts WHERE id = '$id'";
+            $query = $this->db->query($sql);
+            return $query;
 	}
 
 	public function get_count_post() {
